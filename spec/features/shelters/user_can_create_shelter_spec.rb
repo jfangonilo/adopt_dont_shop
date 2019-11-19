2d8b65d71ecd0acb39afa13bbeccb51e26bf0898
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-19 15:03:48 -0700

    Add tests to new shelter page

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -1,1 +1,14 @@
 require 'rails_helper'
+
+RSpec.describe "add shelter page", type: :feature do
+  it "can add a shelter" do
+    visit "shelters/new"
+
+  expect(page).to have_field "shelter[name]"
+  expect(page).to have_field "shelter[address]"
+  expect(page).to have_field "shelter[city]"
+  expect(page).to have_field "shelter[state]"
+  expect(page).to have_field "shelter[zip]"
+  expect(page).to have_button "submit"
+  end
+end

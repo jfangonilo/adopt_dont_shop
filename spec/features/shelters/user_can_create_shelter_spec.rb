8a2da0b89337dca55745668854d202a344e8c08b
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-19 16:44:51 -0700

    Add test for new shelter link

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "add shelter page", type: :feature do
+  it "has link for new shelter on index", type: :feature do
+    visit "/shelters"
+
+    click_link 'New Shelter'
+    assert_equal '/shelters/new', current_path
+  end
+  
   it "can add a shelter" do
 
     visit "shelters/new"
@@ -11,6 +18,6 @@ RSpec.describe "add shelter page", type: :feature do
     fill_in 'shelter[zip]', with: 'test shelter zip'
     click_button 'submit'
 
-    assert_equal '/shelters', page.current_path
+    assert_equal '/shelters', current_path
   end
 end

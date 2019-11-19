3779f2657fefafb5b7f87238d8555dec45a1506d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-19 11:51:33 -0700

    Add test to see individual shelter

---
+++ spec/features/shelters/user_can_see_a_shelter_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "shelter show page", type: :feature do
+  it "can see a single shelter" do
+    shelter_1 = Shelter.create(name:    "Shelter 1",
+                               address: "6303 W Exposition Ave",
+                               city:    "Lakewood",
+                               state:   "CO",
+                               zip:     "80226")
+
+    visit 'shelters/#{shelter_1.id}'
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_1.address)
+    expect(page).to have_content(shelter_1.city)
+    expect(page).to have_content(shelter_1.state)
+    expect(page).to have_content(shelter_1.zip)
+  end
+end

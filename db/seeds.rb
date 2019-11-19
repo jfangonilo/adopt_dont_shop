bb7c3a02ba73e4cee8703adf234b706276610bdd
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-19 11:42:49 -0700

    Add shelter seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Shelter.create(name:    "Shelter 1",
+               address: "6303 W Exposition Ave",
+               city:    "Lakewood",
+               state:   "CO",
+               zip:     "80226")
+Shelter.create(name:    "Shelter 2",
+               address: "12322 Bohannon Blvd",
+               city:    "Orlando",
+               state:   "FL",
+               zip:     "32824")

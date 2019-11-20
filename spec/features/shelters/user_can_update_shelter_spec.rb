89c10d52bf63a945dc7a645e347ea6853285645d
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-20 00:50:24 -0700

    Add test for update and redirect

--- spec/features/shelters/user_can_update_shelter_spec.rb
+++ spec/features/shelters/user_can_update_shelter_spec.rb
@@ -13,7 +13,20 @@ RSpec.describe "update shelter page", type: :feature do
     assert_equal "/shelters/#{shelter_1.id}/edit", current_path
   end
 
-  # it "can update shelter from edit page and redirects" do
-  #
-  # end
+  it "can update shelter from edit page and redirects" do
+    shelter_1 = Shelter.create(name:    "Shelter 1",
+                   address: "6303 W Exposition Ave",
+                   city:    "Lakewood",
+                   state:   "CO",
+                   zip:     "80226")
+
+    visit "/shelters/#{shelter_1.id}/edit"
+    fill_in 'shelter[name]', with: 'test shelter'
+    fill_in 'shelter[address]', with: 'test shelter address'
+    fill_in 'shelter[city]', with: 'test shelter city'
+    fill_in 'shelter[state]', with: 'test shelter state'
+    fill_in 'shelter[zip]', with: 'test shelter zip'
+    click_button "submit"
+    assert_equal "/shelters/#{shelter_1.id}", current_path
+  end
 end

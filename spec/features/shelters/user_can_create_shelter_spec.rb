8642dabb21e349f91bd131d9efc0aae38340f0cc
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-20 00:50:05 -0700

    Fix formatting

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe "add shelter page", type: :feature do
     click_link 'New Shelter'
     assert_equal '/shelters/new', current_path
   end
-  
+
   it "can add a shelter" do
 
     visit "shelters/new"
-    fill_in 'shelter[name]', with: 'test shelter'
+    fill_in 'shelter[name]',    with: 'test shelter'
     fill_in 'shelter[address]', with: 'test shelter address'
-    fill_in 'shelter[city]', with: 'test shelter city'
-    fill_in 'shelter[state]', with: 'test shelter state'
-    fill_in 'shelter[zip]', with: 'test shelter zip'
+    fill_in 'shelter[city]',    with: 'test shelter city'
+    fill_in 'shelter[state]',   with: 'test shelter state'
+    fill_in 'shelter[zip]',     with: 'test shelter zip'
     click_button 'submit'
 
     assert_equal '/shelters', current_path

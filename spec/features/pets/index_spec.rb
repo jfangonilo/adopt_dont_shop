baa221ca4b6048c74e5f1c8c4a501f71fdf173f5
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-20 16:35:27 -0700

    Add pet model tests

---
+++ spec/features/pets/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "pets index page", type: :feature do
+  before :each do
+    @shelter_1 = Shelter.create(name: "Shelter 1",
+      address: "6303 W Exposition Ave",
+      city:    "Lakewood",
+      state:   "CO",
+      zip:     "80226")
+    @shelter_2 = Shelter.create(name: "Shelter 2",
+      address: "12322 Bohannon Blvd",
+      city:    "Orlando",
+      state:   "FL",
+      zip:     "32824")
+
+    @pet_1 = {image:    "string",
+      name:             "Seamus",
+      approximate_age:  8,
+      sex:              "M",
+      shelter_id:       @shelter_1.id}
+    @pet_2 = {image:    "string",
+      name:             "Pepper",
+      approximate_age:  4,
+      sex:              "F",
+      shelter_id:       @shelter_2.id}
+  end
+
+  it "displays pet info" do
+
+  end
+end

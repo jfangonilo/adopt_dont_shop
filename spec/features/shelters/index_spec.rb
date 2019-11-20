e59836307d0e39ddaa9191c124d6003cb3382d24
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-20 14:36:12 -0700

    Rename index tests

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "shelters index", type: :feature do
+describe "shelters index page", type: :feature do
   before :each do
     @shelter_1 = Shelter.create(name: "Shelter 1")
     @shelter_2 = Shelter.create(name: "Shelter 2")
@@ -13,17 +13,17 @@ describe "shelters index", type: :feature do
     expect(page).to have_content(@shelter_2.name)
   end
 
-  it "can visit individual shelters" do
+  it "can navigate to individual shelters" do
     click_link "#{@shelter_1.name}"
     assert_equal "/shelters/#{@shelter_1.id}", current_path
   end
 
-  it "can access new shelter page" do
+  it "can navigate to new shelter page" do
     click_link "New Shelter"
     assert_equal "/shelters/new", current_path
   end
 
-  xit "can visit the edit page for a shelter" do
+  xit "can navigate to a specific shelter edit page" do
     # within 'shelter' do
       click_link "Edit"
       assert_equal "/shelter/#{@shelter_1.id}/edit", current_path

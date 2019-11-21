1a30063a12e117679d37720d6984d93750533a07
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-21 14:27:07 -0700

    Refactor new shelter page

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe "add shelter page", type: :feature do
-  it "can add a shelter" do
+describe "As a visitor, when I visit shelters/new," do
+  it "I can fill out a form to create a shelter and am returned to the shelter index page" do
     visit "shelters/new"
     fill_in 'shelter[name]',    with: 'test shelter'
     fill_in 'shelter[address]', with: 'test shelter address'
@@ -10,6 +10,7 @@ describe "add shelter page", type: :feature do
     fill_in 'shelter[zip]',     with: 'test shelter zip'
     click_button 'submit'
 
-    assert_equal '/shelters', current_path
+    expect(current_path).to eq "/shelters"
+    expect(page).to have_content "test shelter"
   end
 end

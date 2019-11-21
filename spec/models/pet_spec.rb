36516596a1ac496d985795c6f9d2fa5d786607e4
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-21 14:23:55 -0700

    Add pet model validations

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -19,7 +19,10 @@ describe Pet, type: :model do
   end
 
   describe "validations" do
+    it {should validate_presence_of :image}
     it {should validate_presence_of :name}
+    it {should validate_presence_of :approximate_age}
+    it {should validate_presence_of :sex}
   end
 
   describe "relationships" do

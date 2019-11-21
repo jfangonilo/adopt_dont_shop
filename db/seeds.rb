fa035adf791ffba74a13ad48139a7858523de4eb
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-20 20:00:39 -0700

    Replace shelter seeds with pet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,14 @@
-Shelter.create(name:    "Shelter 1",
-               address: "6303 W Exposition Ave",
-               city:    "Lakewood",
-               state:   "CO",
-               zip:     "80226")
-Shelter.create(name:    "Shelter 2",
-               address: "12322 Bohannon Blvd",
-               city:    "Orlando",
-               state:   "FL",
-               zip:     "32824")
+Pet.create(
+  image:            "link",
+  name:             "Seamus",
+  approximate_age:  8,
+  sex:              "M",
+  shelter_id:       1
+)
+Pet.create(
+  image:            "link/2",
+  name:             "Pepper",
+  approximate_age:  4,
+  sex:              "F",
+  shelter_id:       2
+)

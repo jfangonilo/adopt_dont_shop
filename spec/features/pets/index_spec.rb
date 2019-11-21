ffb8817ac0d8d37622c4a8e5df8a325dff495f51
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-21 14:30:49 -0700

    Refactor pets index tests

--- spec/features/pets/index_spec.rb
+++ spec/features/pets/index_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe "pets index page", type: :feature do
+describe "As a user, when I visit the pet index page,", type: :feature do
   before :each do
     @shelter_1 = Shelter.create(
       name:     "Shelter 1",
@@ -18,34 +18,34 @@ describe "pets index page", type: :feature do
     )
 
     @pet_1 = Pet.create(
-      image:            "string",
+      image:            "string 1",
       name:             "Seamus",
       approximate_age:  8,
-      sex:              "M",
+      sex:              "male",
       shelter_id:       @shelter_1.id
     )
     @pet_2 = Pet.create(
-      image:            "string",
+      image:            "string 2",
       name:             "Pepper",
       approximate_age:  4,
-      sex:              "F",
+      sex:              "female",
       shelter_id:       @shelter_2.id
     )
   end
 
-  it "displays pet info" do
+  it "I can see all pets and their image, name, age, sex, name of shelter" do
     visit "/pets"
 
-    expect(page).to have_content(@pet_1.image)
-    expect(page).to have_content(@pet_1.name)
-    expect(page).to have_content(@pet_1.approximate_age)
-    expect(page).to have_content(@pet_1.sex)
-    expect(page).to have_content(@pet_1.shelter_name)
+    expect(page).to have_content("string 1")
+    expect(page).to have_content("Seamus")
+    expect(page).to have_content(8)
+    expect(page).to have_content("male")
+    expect(page).to have_content("Shelter 1")
 
-    expect(page).to have_content(@pet_2.image)
-    expect(page).to have_content(@pet_2.name)
-    expect(page).to have_content(@pet_2.approximate_age)
-    expect(page).to have_content(@pet_2.sex)
-    expect(page).to have_content(@pet_2.shelter_name)
+    expect(page).to have_content("string 2")
+    expect(page).to have_content("Pepper")
+    expect(page).to have_content(4)
+    expect(page).to have_content("female")
+    expect(page).to have_content("Shelter 2")
   end
 end

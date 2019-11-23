0e587556c9ac8d6633ae5d52adc8e0e309a743ff
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-22 22:46:33 -0700

    Add test for shelter delete from index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -38,7 +38,14 @@ describe "As a visitor, when I visit the shelters index page," do
   it "I can navigate to a specific shelter edit page" do
     within "#shelter-#{@shelter_1.id}" do
       click_link "Edit"
-      assert_equal "/shelters/#{@shelter_1.id}/edit", current_path
     end
+    expect(current_path).to eq "/shelters/#{@shelter_1.id}/edit"
+  end
+
+  it "I can delete a shelter" do
+    within "#shelter-#{@shelter_1.id}" do
+      click_link "Delete"
+    end
+    expect(page).to_not have_content "Shelter 1"
   end
 end

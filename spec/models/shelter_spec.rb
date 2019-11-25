48e594f748877e4d74da8413985e7af2bef69398
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-24 18:51:52 -0700

    Add test for pet_count

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,6 +1,31 @@
 require 'rails_helper'
 
 describe Shelter, type: :model do
+  before :each do
+    @shelter_1 = Shelter.create!(
+      name:     "House of Seamus",
+      address:  "6303 W Exposition Ave",
+      city:     "Lakewood",
+      state:    "CO",
+      zip:      "80226"
+    )
+
+    @pet_1 = @shelter_1.pets.create!(
+      image:            "string",
+      name:             "Seamus",
+      approximate_age:  8,
+      sex:              "M",
+      description:      "Winter is Coming, dog"
+    )
+    @pet_2 = @shelter_1.pets.create(
+      image:            "https://www.catster.com/wp-content/uploads/2018/03/Calico-cat.jpg",
+      name:             "Miso",
+      approximate_age:  2,
+      sex:              "female",
+      description:      "Winter is Coming; cat"
+    )
+  end
+
   describe "validations" do
     it {should validate_presence_of :name}
     it {should validate_presence_of :address}
@@ -11,5 +36,11 @@ describe Shelter, type: :model do
 
   describe "relationships" do
     it {should have_many :pets}
+  end
+
+  describe "method" do
+    it ".pet_count" do
+      expect(@shelter_1.pet_count).to eq 2
+    end
   end
 end

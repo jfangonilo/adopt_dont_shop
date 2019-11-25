6fb9191710ceac7919a6542bab6103e0d9a1a6ec
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-25 08:26:32 -0700

    Change test to target sections

--- spec/features/pets/show_spec.rb
+++ spec/features/pets/show_spec.rb
@@ -48,14 +48,19 @@ describe "When I visit /pets/:id," do
   end
 
   it "I can click a link to change the pet's status to Adoption Pending/Adoptable" do
+
     click_link "Change to Adoption Pending"
     expect(current_path).to eq "/pets/#{@pet.id}"
-    expect(page).to have_content "Adoption Pending"
-    expect(page).to_not have_content "Adoptable"
+    within "#adoptable-status" do
+      expect(page).to have_content "Adoption Pending"
+      expect(page).to_not have_content "Adoptable"
+    end
 
     click_link "Change to Adoptable"
     expect(current_path).to eq "/pets/#{@pet.id}"
-    expect(page).to_not have_content "Adoption Pending"
-    expect(page).to have_content "Adoptable"
+    within "#adoptable-status" do
+      expect(page).to_not have_content "Adoption Pending"
+      expect(page).to have_content "Adoptable"
+    end
   end
 end

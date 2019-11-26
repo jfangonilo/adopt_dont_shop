570b5b107c19914f1cb1b3597dd709123a162936
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2019-11-26 10:07:19 -0700

    Add test for alpha sort

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe "As a visitor, when I visit the shelters index page," do
   before :each do
     @shelter_1 = Shelter.create!(
-      name:     "Shelter 1",
+      name:     "Winterfell",
       address:  "6303 W Exposition Ave",
       city:     "Lakewood",
       state:    "CO",
       zip:      "80226"
     )
     @shelter_2 = Shelter.create!(
-      name:     "Shelter 2",
+      name:     "Sunspear",
       address:  "12322 Bohannon Blvd",
       city:     "Orlando",
       state:    "FL",
@@ -21,8 +21,8 @@ describe "As a visitor, when I visit the shelters index page," do
   end
 
   it "I can see the name of each shelter in the system" do
-    expect(page).to have_content "Shelter 1"
-    expect(page).to have_content "Shelter 2"
+    expect(page).to have_content "Winterfell"
+    expect(page).to have_content "Sunspear"
   end
 
   it "I can navigate to the shelter creation page via a link" do
@@ -31,7 +31,7 @@ describe "As a visitor, when I visit the shelters index page," do
   end
 
   it "I can navigate to individual shelters via a link" do
-    click_link "Shelter 1"
+    click_link "Winterfell"
     expect(current_path).to eq "/shelters/#{@shelter_1.id}"
   end
 
@@ -46,6 +46,12 @@ describe "As a visitor, when I visit the shelters index page," do
     within "#shelter-#{@shelter_1.id}" do
       click_link "Delete"
     end
-    expect(page).to_not have_content "Shelter 1"
+    expect(page).to_not have_content "Winterfell"
+  end
+
+  it "I can sort alphhabetically" do
+    click_link "Sort Alphabetically"
+    expect(page.body.index("Sunspear")).to be < page.body.index("Winterfell")
   end
 end
+
